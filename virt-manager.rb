7ea905fe34f4ae90a1c80d085fc9190326609030
eszio <eakasak@gmail.com>
2022-04-19 17:07:26 +0700

    Fix chardet URL

--- virt-manager.rb
+++ virt-manager.rb
@@ -42,7 +42,7 @@ class VirtManager < Formula
   end
 
   resource "chardet" do
-    url "hhttps://files.pythonhosted.org/packages/1b/a5/4eab74853625505725cefdf168f48661b2cd04e7843ab836f3f63abf81da/urllib3-1.26.9.tar.gz"
+    url "https://files.pythonhosted.org/packages/1b/a5/4eab74853625505725cefdf168f48661b2cd04e7843ab836f3f63abf81da/urllib3-1.26.9.tar.gz"
     sha256 "0d6f53a15db4120f2b08c94f11e7d93d2c911ee118b6b30a04ec3ee8310179fa"
   end
 
