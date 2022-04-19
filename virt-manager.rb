4490c0a0a8ad32cc10f772348065c59044d1ff30
eszio <eakasak@gmail.com>
2022-04-19 17:12:27 +0700

    Update hash urllib3

--- virt-manager.rb
+++ virt-manager.rb
@@ -48,7 +48,7 @@ class VirtManager < Formula
 
   resource "urllib3" do
     url "https://files.pythonhosted.org/packages/1b/a5/4eab74853625505725cefdf168f48661b2cd04e7843ab836f3f63abf81da/urllib3-1.26.9.tar.gz"
-    sha256 "0d6f53a15db4120f2b08c94f11e7d93d2c911ee118b6b30a04ec3ee8310179fa"
+    sha256 "aabaf16477806a5e1dd19aa41f8c2b7950dd3c746362d7e3223dbe6de6ac448e"
   end
 
   resource "requests" do
