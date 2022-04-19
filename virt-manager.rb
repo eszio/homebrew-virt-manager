66698f6a01040c01f7fc8b87b100e2eaf8b6d07f
eszio <eakasak@gmail.com>
2022-04-19 17:20:05 +0700

    Chardet wrong URL

--- virt-manager.rb
+++ virt-manager.rb
@@ -42,7 +42,7 @@ class VirtManager < Formula
   end
 
   resource "chardet" do
-    url "https://files.pythonhosted.org/packages/1b/a5/4eab74853625505725cefdf168f48661b2cd04e7843ab836f3f63abf81da/urllib3-1.26.9.tar.gz"
+    url "https://files.pythonhosted.org/packages/ee/2d/9cdc2b527e127b4c9db64b86647d567985940ac3698eeabc7ffaccb4ea61/chardet-4.0.0.tar.gz"
     sha256 "0d6f53a15db4120f2b08c94f11e7d93d2c911ee118b6b30a04ec3ee8310179fa"
   end
 
